1c6f239f73e9b3edd65c15ba8419f99241ac579d
tfeydev <tfeydeveloper@gmail.com>
2024-12-05 13:31:21 -0300

    update new

---
+++ frontend/src/app/components/data-preview/data-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-preview.component.html',
+  styleUrl: './data-preview.component.scss'
+})
+export class DataPreviewComponent {
+
+}
