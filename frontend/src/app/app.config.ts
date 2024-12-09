4f55442ff77e06bd90a56f833096893c739df481
tfeydev <tfeydeveloper@gmail.com>
2024-12-09 11:48:31 -0300

    update

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,23 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { DataSourceManagerComponent } from './data-source-manager/data-source-manager.component';
+import { AnalyticsHubComponent } from './analytics-hub/analytics-hub.component';
+import { MonitoringComponent } from './analytics-hub/components/monitoring/monitoring.component';
+import { AnalysisComponent } from './analytics-hub/components/analysis/analysis.component';
+import { ReportsComponent } from './analytics-hub/components/reports/reports.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter([
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'data-source-manager', component: DataSourceManagerComponent },
+      { path: 'analytics-hub', component: AnalyticsHubComponent,
+        children: [
+          { path: 'monitoring', component: MonitoringComponent },
+          { path: 'analysis', component: AnalysisComponent },
+          { path: 'reports', component: ReportsComponent },
+        ]
+      },
     ]),
     provideHttpClient(),
   ],
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'; 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { provideServerRendering } from '@angular/platform-server';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withFetch()),
+    provideServerRendering(),
     provideRouter(routes),
     provideAnimations(), // Enable animations here
   ],
